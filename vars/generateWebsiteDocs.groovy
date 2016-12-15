da62b6a853ffe695f8faa868ed663d68854a7bbd
Kamesh Sampath <kamesh.sampath@hotmail.com>
2016-12-15 16:13:23 +0530

    run doc commands in maven container

--- vars/generateWebsiteDocs.groovy
+++ vars/generateWebsiteDocs.groovy
@@ -16,28 +16,32 @@ def call(body) {
 
     def gitRepoUrl = "https://github.com/${config.project}.git"
 
-    checkout scm: [$class          : 'GitSCM',
-                   useRemoteConfigs: [[url: gitRepoUrl]],
-                   branches        : [[name: "refs/tags/v${docVersion}"]]],
-            changelog: false, poll: false
+    container(name: 'maven') {
 
-    if (docgenScript == null) {
-        if (profiles == null) {
-            sh 'mvn -Pdoc-html,doc-pdf'
+        checkout scm: [$class          : 'GitSCM',
+                       useRemoteConfigs: [[url: gitRepoUrl]],
+                       branches        : [[name: "refs/tags/v${docVersion}"]]],
+                changelog: false, poll: false
+
+        if (docgenScript == null) {
+            if (profiles == null) {
+                sh 'mvn -Pdoc-html,doc-pdf'
+            } else {
+                def argProfile = '-P' + profiles.join(",")
+                sh "mvn ${argProfile}"
+            }
+            //TODO - check if gh-pages already exist if not create it ??
+            sh 'git clone -b gh-pages' + gitRepoUrl + ' gh-pages'
+            sh 'cp -rv target/generated-docs/* gh-pages/'
+            sh 'cd gh-pages'
+            sh 'mv index.pdf ' + artifactId + '.pdf'
+            sh 'git add --ignore-errors *'
+            sh 'git commit -m "generated documentation'
+            sh 'git push origin gh-pages'
         } else {
-            def argProfile = '-P' + profiles.join(",")
-            sh "mvn ${argProfile}"
+            sh "${docgenScript}"
         }
-        //TODO - check if gh-pages already exist if not create it ??
-        sh 'git clone -b gh-pages' + gitRepoUrl + ' gh-pages'
-        sh 'cp -rv target/generated-docs/* gh-pages/'
-        sh 'cd gh-pages'
-        sh 'mv index.pdf ' + artifactId + '.pdf'
-        sh 'git add --ignore-errors *'
-        sh 'git commit -m "generated documentation'
-        sh 'git push origin gh-pages'
-    } else {
-        sh "${docgenScript}"
+
     }
 
 }
